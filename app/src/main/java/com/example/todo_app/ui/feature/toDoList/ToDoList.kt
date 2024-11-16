c085ee175aeaeb28a95383bb9c740655a23e1569
Toby <60399231+TobyNatooor@users.noreply.github.com>
2024-11-16 21:42:56 +0100

    added checkbox to todos

--- app/src/main/java/com/example/todo_app/ui/feature/toDoList/ToDoList.kt
+++ app/src/main/java/com/example/todo_app/ui/feature/toDoList/ToDoList.kt
@@ -3,6 +3,7 @@ package com.example.todo_app.ui.feature.toDoList
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +11,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.text.font.FontWeight
@@ -17,13 +20,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.lifecycle.ViewModel
 
 import com.example.todo_app.model.ToDo
 
 
 @Composable
-fun ToDoList(toDos: List<ToDo>, modifier: Modifier = Modifier) {
+fun ToDoList(toDos: List<ToDo>, viewmodel: ToDoListViewModel, modifier: Modifier = Modifier) {
     val scrollState = rememberLazyListState()
 
     LazyColumn(
@@ -32,13 +38,13 @@ fun ToDoList(toDos: List<ToDo>, modifier: Modifier = Modifier) {
             .fillMaxSize()
     ) {
         itemsIndexed(toDos) { index, toDo ->
-            ToDoItem(toDo = toDo, index = index)
+            ToDoItem(viewmodel, toDo = toDo, index = index)
         }
     }
 }
 
 @Composable
-private fun ToDoItem(toDo: ToDo, index: Int = 0) {
+private fun ToDoItem(viewmodel: ToDoListViewModel, toDo: ToDo, index: Int = 0) {
     Box(
         modifier = Modifier
             .padding(horizontal = 16.dp, vertical = 8.dp)
@@ -49,10 +55,15 @@ private fun ToDoItem(toDo: ToDo, index: Int = 0) {
             )
             .padding(16.dp) // Inner padding for the content inside the box
     ) {
-        Text(
-            text = toDo.title,
-            fontSize = 20.sp,
-            color = Color.Black
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(toDo.isDone, onCheckedChange = {
+                viewmodel.updateToDoItem(toDo.copy(isDone = !toDo.isDone))
+            })
+            Text(
+                text = toDo.title,
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+        }
     }
 }
