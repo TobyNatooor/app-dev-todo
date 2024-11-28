11495f32f2a21becc657610d96f09eb661b0d2df
MarcusRLE <s230985@dtu.dk>
2024-11-28 13:11:06 +0100

    Data is continuous through sessions

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -23,8 +23,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Should only be used for debugging. Not for release!
-        applicationContext.deleteDatabase("ToDoDB")
+        // UNCOMMENT WHEN TESTING
+        //applicationContext.deleteDatabase("ToDoDB")
 
         val db = Room.databaseBuilder(
             applicationContext,
@@ -34,7 +34,10 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         lifecycleScope.launch(Dispatchers.IO) {
-            MockDataStore().insertMockData(db)
+            // Should not be in release
+            if(db.toDoDao().numberOfToDos() == 0) {
+                MockDataStore().insertMockData(db)
+            }
             withContext(Dispatchers.Main){
                 setContent {
                     TodoappTheme {
