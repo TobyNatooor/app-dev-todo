c76c92fa6309cfc2287cf6ff7ee4b472343a85aa
MarcusRLE <s230985@dtu.dk>
2025-01-07 09:50:43 +0100

    TextField and hinttext

--- app/src/main/java/com/example/todo_app/ui/feature/home/HomeList.kt
+++ app/src/main/java/com/example/todo_app/ui/feature/home/HomeList.kt
@@ -1,6 +1,7 @@
 package com.example.todo_app.ui.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,13 +13,23 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -64,7 +75,7 @@ fun HomeList(
 private fun ListCard(list: CheckList, viewModel: HomeViewModel) {
     return Card(
         onClick = {
-            viewModel.clickList(listTitle = list.title, listId = list.id)
+            viewModel.clickList(listTitle = list.title.toString(), listId = list.id)
         },
         modifier = Modifier
             .width(130.dp)
@@ -72,13 +83,64 @@ private fun ListCard(list: CheckList, viewModel: HomeViewModel) {
     ) {
         Column(modifier = Modifier.padding(10.dp, 10.dp)) {
             Row {
-                Text(list.title, fontSize = 20.sp)
+                if(list.title != null){
+                    Text(list.title.toString(), fontSize = 20.sp)
+                } else {
+                    ListTextField(list, viewModel)
+                }
                 Spacer(modifier = Modifier.weight(1f))
                 Icon(
                     Icons.Rounded.MoreVert,
                     contentDescription = null,
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun ListTextField(list: CheckList, viewModel: HomeViewModel){
+    val focusRequester = remember { FocusRequester() }
+    var isFocused by remember { mutableStateOf(false) }
+    val blankTitle = "Unnamed list"
+    var title by remember { mutableStateOf("") }
+    Box(){
+        BasicTextField(
+            value = title,
+            onValueChange = { newTitle ->
+                title = newTitle
+            },
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 0.dp)
+                .focusRequester(focusRequester)
+                // Handle title update to Room SQL when unfocused
+                .onFocusChanged {
+                    isFocused = !isFocused
+                    if (!isFocused) {
+                        if (title.isBlank()) {
+                            title = blankTitle
+                        }
+                        viewModel.updateList(
+                            list.copy(title = title)
+                        )
+                        //isEnabled = false
+                    }
+                },
+        )
+
+        // Hint text when title is blank
+        if (title.isBlank()) {
+            Text(
+                text = "Enter new title",
+                color = Color.Gray,
+                fontSize = 20.sp,
+            )
         }
     }
 }
