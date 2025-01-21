3d2d358ba695ceda27ac9d78a690432b8335dae7
MarcusRLE <s230985@dtu.dk>
2025-01-21 12:22:56 +0100

    On delete cascade is done

--- app/src/main/java/com/example/todo_app/model/ToDo.kt
+++ app/src/main/java/com/example/todo_app/model/ToDo.kt
@@ -1,16 +1,29 @@
 package com.example.todo_app.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
-@Entity(tableName = "ToDo")
+@Entity(
+    tableName = "ToDo",
+    foreignKeys = [
+        ForeignKey(
+            entity = CheckList::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("listId"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )]
+)
 data class ToDo(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val description: String,
     val created: LocalDateTime = LocalDateTime.now(),
+    @ColumnInfo(name = "listId")
     val listId: Int,
     val location: String? = null,
     val latitude: Double? = null,
