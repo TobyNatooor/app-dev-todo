ecbd5ca244cb14e1cd24ee73795ac324cc050491
BjarneBjarne <s235107@dtu.dk>
2025-01-22 13:53:45 +0100

    in-commented 4 lines in main activity

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -68,10 +68,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         lifecycleScope.launch(Dispatchers.IO) {
             // Should not be in release
-//            if (db.toDoDao().numberOfToDos() == 0) {
-//                MockDataStore().insertMockData(db)
-//                Log.d("TESTING", "onCreate: ${db.toDoDao().numberOfToDos()}")
-//            }
+            if (db.toDoDao().numberOfToDos() == 0) {
+                MockDataStore().insertMockData(db)
+                Log.d("TESTING", "onCreate: ${db.toDoDao().numberOfToDos()}")
+            }
             withContext(Dispatchers.Main) {
                 setContent {
                     TodoappTheme {
